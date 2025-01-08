c9f0e17918e13d57b190e842b61ba6fa67acb24d
MANU-9999 <chmanojkumar9999@gmail.com>
2025-01-08 22:43:56 +0530

    Added print statement

--- src/test/java/tests/TestA.java
+++ src/test/java/tests/TestA.java
@@ -5,13 +5,13 @@ import org.testng.annotations.Test;
 import PageObjects.UsersPage;
 import testSetup.Base;
 
-public class Test2 extends Base {
-
+public class TestA extends Base {
 	@Test
 	public void user() {
 
 		UsersPage u = new UsersPage();
 		u.NavigateToUsers();
+		System.out.println("start");
 	}
 
 }
--- src/test/java/tests/TestB.java
+++ src/test/java/tests/TestB.java
@@ -5,13 +5,13 @@ import org.testng.annotations.Test;
 import PageObjects.UsersPage;
 import testSetup.Base;
 
-public class Test1 extends Base {
+public class TestB extends Base {
+
 	@Test
 	public void user() {
 
 		UsersPage u = new UsersPage();
 		u.NavigateToUsers();
-		System.out.println("start");
 	}
 
 }
